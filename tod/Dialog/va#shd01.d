a8c841eef7c6bd0c1b57aa85ed14e39bee1fc8c2
ALIENQuake <alienquake@hotmail.com>
2020-05-22 10:37:16 +0200

    renamed

--- tod/Dialog/va#shd01.d
+++
@@ -1,6 +0,0 @@
-BEGIN va#shd01
-
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN FloorEntered // party just entered the floor
-  SAY @0
-  IF ~~ THEN DO ~Enemy()~ EXIT
-END
--- tod/Dialog/va#tian2.d
+++
@@ -1,7 +0,0 @@
-BEGIN va#tian2
-
-IF ~NumTimesTalkedTo(0)~ THEN BEGIN FirstMeet // Party´s first meeeting with Tian #2 outside of Athkatla
-  SAY @0
-  IF ~~ THEN DO ~StartCutSceneMode()
-                 StartCutScene("va#03sc")~ EXIT
-END
